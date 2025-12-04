a86d4a6b91b362a26fd5b21e8ef5767dc2ebedfc
JackMilner1 <jackellismilner@gmail.com>
2025-12-04 23:16:00 +0000

    character unit test

--- Character.py
+++ Character.py
@@ -7,3 +7,25 @@ class Character:
     
     def __str__(self):
         return f'--------------------\nName: {self.name}\n--------------------\nSkills: {self.skills}\nTraits: {self.traits}\n--------------------'
+    
+if __name__ == "__main__":
+    
+    didSucceed = []
+    testChar = None
+    testNum = 0
+
+    try:
+        testChar = Character("Jack",["Construction: +2","Science: +9"], ["Artist: This character can play guitar"])
+        didSucceed.append(("can create obj",True))
+        print(testChar)
+        didSucceed.append(("can __str__",True))
+    except:
+        print("Not all tests succeeded")
+        
+    print("\n\n\n============== CHARACTER UNIT TEST ============== ")
+    print("=================================================")
+        
+    for i in range(len(didSucceed)):
+        print(f"TEST {i} ({didSucceed[i][0]}) RESULT ==> {"Success" if didSucceed[i][1] else "Faliure"}")
+    
+    print("=================================================")
