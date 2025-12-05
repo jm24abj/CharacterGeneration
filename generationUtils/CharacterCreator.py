c95f168916818efc2ecf91530e92cd41af0a50d2
JackMilner1 <jackellismilner@gmail.com>
2025-12-05 17:02:20 +0000

    migrating to one file
    functions are small enough to put into one file

--- generationUtils/CharacterCreator.py
+++ generationUtils/CharacterCreator.py
@@ -1,6 +1,4 @@
-import attributeGenerator
-import characterSheetGenerator
-
+import random
 '''  ============================ INFO ============================
 
 character creator is a seperate class than the character class
@@ -19,14 +17,42 @@ class CharacterCreator:
     def randomise(self,KEEP_NAME: bool, KEEP_SKILLS_AND_TRAITS: bool):
 
         if not KEEP_NAME:
-            self.name = attributeGenerator.generateName()
+            self.name = self.generateName()
         
         if not KEEP_SKILLS_AND_TRAITS:
-            self.skills,self.traits = characterSheetGenerator.generateSkillsAndTraits()
-    
+            self.skills,self.traits = self.generateSkillsAndTraits()
+
+    def generateName(self):
+        NAME_LIST = [
+            "Alice", "Benjamin", "Chloe", "Daniel", "Emma",
+            "Finn", "Grace", "Henry", "Isla", "Jack",
+            "Lily", "Mason", "Nora", "Oliver", "Poppy",
+            "Quinn", "Ruby", "Samuel", "Tara", "Uma",
+            "Violet", "William", "Xavier", "Yara", "Zoe"
+        ]
+
+        return NAME_LIST[random.randint(0,len(NAME_LIST)-1)]
+        
+
+    def selectSkill(self):
+        return ""
+
+    def generateAttributes(skillPoints,maxSkills):
+        while skillPoints != 0 and maxSkills > 0:
+            pass
+   
     def setCustomName(self,name : str):
         self.name = name
 
+    def generateSkills(self):
+        return ""
+
+    def generateTraits(self):
+        return ""
+
+    def generateSkillsAndTraits(self):
+        return self.generateSkills(),self.generateTraits()
+    
     def finishCustomisation(self) -> tuple :
         return self.name,self.skills,self.traits
 
