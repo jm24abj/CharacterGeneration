fd0fbeaa66004562cee8d2afb4ad6733cb2764b9
JackMilner1 <jackellismilner@gmail.com>
2025-12-05 17:02:43 +0000

    character generation menu
    menu works fully

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+from generationUtils import CharacterCreator
+
 # Holds GUI for the project
 
 def run():
-    pass
+    finishedGenerating = False
+    character = CharacterCreator.CharacterCreator()
+
+    while not finishedGenerating:
+        print(character)
+
+        option = -1
+
+        while option < 1 or option > 5: 
+            print("Select an option\n1. re-roll all\n2. re-roll name\n3. re-roll character sheet\n4. enter custom name\n5. finish")
+            option = int(input("Input: "))
+
+        
+            if option == 1:
+                character.randomise(False,False)
+            elif option == 2:
+                character.randomise(False,True)
+            elif option == 3:
+                character.randomise(True,False)
+            elif option == 4:
+                newName = input("\nEnter a new name for your character: ")
+                character.setCustomName(newName)
+            elif option == 5:
+                finishedGenerating = True
+            else:
+                print("\nnot a valid option!")
 
 if __name__ == "__main__":
     run()
